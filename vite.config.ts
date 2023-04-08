be81763bfa27372d426ef20d64f4d637dd51e41c
Hussein Najah <husseinnajah123@gmail.com>
2023-04-08 22:11:12 +0300

    vue

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import UnoCss from 'unocss/vite'
+import { presetUno, presetAttributify } from 'unocss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), UnoCss({ presets: [presetUno(), presetAttributify()] })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
