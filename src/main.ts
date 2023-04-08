06c4b8d0eb7c77b4bf10eeb5b98421ed96f61909
Hussein Najah <husseinnajah123@gmail.com>
2023-04-08 22:38:29 +0300

    formatting and fixing

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import 'uno.css'
 
+import 'uno.css'
 import './assets/main.css'
 
 const app = createApp(App)
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,19 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <button
+      bg="blue-400 hover:blue-500 dark:blue-500 dark:hover:blue-600"
+      text="sm white "
+      font="mono light"
+      p="y-2 x-4"
+      border="2 rounded blue-200"
+    >
+      Button
+    </button>
+    <button
+      class="bg-blue-400 hover:bg-blue-500 text-sm text-white font-mono font-light py-2 px-4 rounded border-2 border-blue-200 dark:bg-blue-500 dark:hover:bg-blue-600"
+    >
+      Button
+    </button>
   </main>
 </template>
+<script setup lang="ts"></script>
