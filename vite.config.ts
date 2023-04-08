360f8f9427861382e6c9926e79963a1d4fdba68b
Hussein Najah <husseinnajah123@gmail.com>
2023-04-08 21:24:14 +0300

    stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import UnoCss from "unocss/vite";
-import { presetUno, presetAttributify, presetIcons } from "unocss";
+import { presetUno, presetAttributify, presetIcons, presetTagify} from "unocss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         vue(),
         UnoCss({
-            presets: [presetUno(), presetAttributify(), presetIcons()],
+            presets: [presetUno(), presetAttributify(), presetIcons(), presetTagify()],
         }),
     ],
 });
