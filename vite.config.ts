66b395e561277a3ad1a482fb644ce1ba3fd127b8
Hussein Najah <husseinnajah123@gmail.com>
2023-04-08 18:18:21 +0300

    all good stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import UnoCss from "unocss/vite";
+import { presetUno, presetAttributify } from "unocss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+    plugins: [
+        vue(),
+        UnoCss({
+            presets: [presetUno(), presetAttributify()],
+        }),
+    ],
+});
